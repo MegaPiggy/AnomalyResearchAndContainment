e2c0be401ad918d54ed88b9fe274e5f6e8be3225
Noah Pilarski <megapiggy@outerwildsmods.com>
2025-07-11 14:05:02 -0400

    lots of progress

---
+++ Jam5Entry/EndTimesHandler.cs
@@ -0,0 +1,41 @@
+﻿using NewHorizons.Handlers;
+using NewHorizons.Utility.OWML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Jam5Entry
+{
+    public static class EndTimesHandler
+    {
+        public static AudioType audioType;
+        public static AudioType normalAudioType;
+        public static OWAudioSource endTimesSource;
+
+        public static void LoadAudio()
+        {
+            audioType = AudioTypeHandler.GetAudioType("planets/EndTimes_Redone.wav", Jam5Entry.Instance);
+        }
+
+        public static void Initialize()
+        {
+            LoadAudio();
+
+            endTimesSource = Locator.GetGlobalMusicController()._endTimesSource;
+            normalAudioType = endTimesSource.audioLibraryClip;
+        }
+
+        public static void Assign()
+        {
+            endTimesSource.AssignAudioLibraryClip(audioType);
+        }
+
+        public static void Unassign()
+        {
+            endTimesSource.AssignAudioLibraryClip(normalAudioType);
+        }
+    }
+}
--- Jam5Entry/Jam5Entry.cs
+++ Jam5Entry/Jam5Entry.cs
@@ -1,7 +1,11 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using Epic.OnlineServices;
 using HarmonyLib;
+using ModJam5;
 using OWML.Common;
 using OWML.ModHelper;
+using UnityEngine;
 
 namespace Jam5Entry
 {
@@ -26,22 +30,40 @@ namespace Jam5Entry
             // Get the New Horizons API and load configs
             NewHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
             NewHorizons.LoadConfigs(this);
-            NewHorizons.GetStarSystemLoadedEvent().AddListener(s =>
-            {
-                if (s == "Jam5")
-                {
-                    var room = NewHorizons.GetPlanet("MegaPiggy_Mod_Platform").transform.Find("Sector/PlatformRoot/Room");
-                    var door = room.transform.Find("Door");
-                    var interact = room.transform.Find("Button/Interact");
-                    interact.gameObject.AddComponent<Jam4JokeEntry.Button>().door = door;
-                }
-            });
+            NewHorizons.GetStarSystemLoadedEvent().AddListener(OnStarSystemLoaded);
 
             new Harmony("MegaPiggy.Jam5Entry").PatchAll(Assembly.GetExecutingAssembly());
 
             // Example of accessing game code.
             OnCompleteSceneLoad(OWScene.TitleScreen, OWScene.TitleScreen); // We start on title screen
             LoadManager.OnCompleteSceneLoad += OnCompleteSceneLoad;
+        }
+
+        private void OnStarSystemLoaded(string name)
+        {
+            if (name == ModJam5.ModJam5.SystemName)
+            {
+                EndTimesHandler.Initialize();
+
+                var star = Jam5Entry.Instance.NewHorizons.GetPlanet("Verdant Beacon");
+                var planet = Jam5Entry.Instance.NewHorizons.GetPlanet("Emerald Reverie");
+                var station = Jam5Entry.Instance.NewHorizons.GetPlanet("Anomaly Station");
+                var platform = Jam5Entry.Instance.NewHorizons.GetPlanet("Anomaly Station Warp Platform");
+
+                var alignment = planet.AddComponent<AlignWithTargetBody>();
+                alignment.SetTargetBody(station.GetComponent<OWRigidbody>());
+                alignment._localAlignmentAxis = Vector3.down;
+                alignment._owRigidbody = planet.GetComponent<OWRigidbody>();
+                var currentDirection = alignment.transform.TransformDirection(alignment._localAlignmentAxis);
+                var targetDirection = alignment.GetAlignmentDirection();
+                alignment.transform.rotation = Quaternion.FromToRotation(currentDirection, targetDirection) * alignment.transform.rotation;
+                alignment._owRigidbody.SetAngularVelocity(Vector3.zero);
+                alignment._usePhysicsToRotate = true;
+
+                var materialReplacer = ModJam5.ModJam5.Instance.GetComponent<StarshipCommunityHelper>();
+                materialReplacer.ReplaceMaterials(station);
+                materialReplacer.ReplaceMaterials(platform);
+            }
         }
 
         public void OnCompleteSceneLoad(OWScene previousScene, OWScene newScene)
